0509310a193850207a6f5af5ba40c1c75590822d
Peter Turner <peter.turner@lifeway.com>
2017-03-18 13:01:13 -0700

    divide input entries into four parts

--- linker.js
+++ linker.js
@@ -10,10 +10,16 @@ const argv = require('yargs')
   .argv;
 const fs = require('fs');
 const path = require('path');
-
 const cwd = process.cwd();
 const src_file_path = path.join(cwd, 'META-INF', argv._[0]);
-
 const src_file_text = fs.readFileSync(src_file_path, 'UTF-8');
-
 const src_entries = src_file_text.split('\r');
+const text_dir_path = path.join(cwd, 'text');
+
+for (let i = 0; i < src_entries.length; i++) {
+  let current_entry = src_entries[i].split('\t');
+  let from_file = current_entry[0];
+  let to_file = current_entry[2];
+  let from_string = current_entry[1].replace(/^"|"$/g, '').replace(/""/g, '\"');
+  let to_html = current_entry[3].replace(/^"|"$/g, '').replace(/""/g, '\"');
+}
